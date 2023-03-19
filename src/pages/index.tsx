5f8efae0aae7193591095707399e1c09a8f513d6
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-03-19 16:59:03 +0900

    🚧 Homeページをスタイリング

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/react';
 import type { NextPage } from 'next';
 import { Description } from '@/components/Description/Description';
 
@@ -9,7 +10,13 @@ const Home: NextPage = () => {
         description="Next.jsをMantine UIと一緒に色々セットアップしてあるオレオレテンプレートリポジトリです。ご利用は計画的に。"
       />
 
-      <div>
+      <div
+        css={css`
+          display: grid;
+          min-height: 100vh;
+          place-content: center;
+        `}
+      >
         <h1>Next.js with Mantine UI Template</h1>
         <p>
           Next.jsをMantine
