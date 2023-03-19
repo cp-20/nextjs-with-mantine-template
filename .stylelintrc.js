31c35ee6d82786356595bec3865996d08534c8be
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-03-19 16:54:54 +0900

    🚧 stylelintとかを色々設定

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,17 +2,20 @@ module.exports = {
   extends: [
     'stylelint-config-standard',
     'stylelint-config-idiomatic-order',
-    'stylelint-prettier/recommended',
+    'stylelint-config-prettier',
   ],
   rules: {
     'string-quotes': 'single',
     'at-rule-no-unknown': null,
     'function-url-quotes': 'always',
     'selector-class-pattern': null,
+    'hue-degree-notation': 'number',
+    'color-function-notation': 'legacy',
+    'alpha-value-notation': 'number',
   },
   overrides: [
     {
-      files: ['**/*.{jsx,tsx}'],
+      files: ['**/*.{js,ts,jsx,tsx}'],
       customSyntax: '@stylelint/postcss-css-in-js',
     },
   ],
