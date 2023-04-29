9f0cb1a6be37c205daf1f0115d9d43551eb6dae7
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-04-29 14:09:45 +0900

    🚧 faviconの設定

--- src/shared/lib/$path.ts
+++ src/shared/lib/$path.ts
@@ -5,7 +5,13 @@ export const pagesPath = {
 export type PagesPath = typeof pagesPath
 
 export const staticPath = {
-  _gitkeep: '/.gitkeep'
+  _gitkeep: '/.gitkeep',
+  apple_touch_icon_png: '/apple-touch-icon.png',
+  favicon_ico: '/favicon.ico',
+  icon_192_png: '/icon-192.png',
+  icon_512_png: '/icon-512.png',
+  icon_svg: '/icon.svg',
+  manifest_webmanifest: '/manifest.webmanifest'
 } as const
 
 export type StaticPath = typeof staticPath
