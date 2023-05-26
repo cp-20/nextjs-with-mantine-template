6b37dc45022c1fd4a9e274a944ae45d32c0a7446
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-05-27 06:10:29 +0900

    🚧 Storybookをとりあえず入れるところまで

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     'next/core-web-vitals',
     'google',
     'prettier',
+    'plugin:storybook/recommended',
   ],
   plugins: [
     'react-hooks',
@@ -26,23 +27,43 @@ module.exports = {
   ],
   rules: {
     'require-jsdoc': ['off'],
-    'import/order': ['error', { alphabetize: { order: 'asc' } }],
+    'valid-jsdoc': ['off'],
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
     '@next/next/no-img-element': ['off'],
-    'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    'react/no-unknown-property': [
+      'error',
+      {
+        ignore: ['css'],
+      },
+    ],
     '@typescript-eslint/no-unused-vars': [
       'error',
-      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+      {
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_',
+      },
     ],
     'no-unused-vars': ['off'],
     '@typescript-eslint/consistent-type-imports': [
       'error',
-      { prefer: 'type-imports' },
+      {
+        prefer: 'type-imports',
+      },
     ],
   },
   overrides: [
     {
       files: ['**/*.d.ts'],
-      rules: { '@typescript-eslint/consistent-type-imports': 'off' },
+      rules: {
+        '@typescript-eslint/consistent-type-imports': 'off',
+      },
     },
   ],
 };
